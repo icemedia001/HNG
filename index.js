c68c22e0e7852cb69e7239f726d015cc6c053e13
Cold Dev <96265196+icemedia001@users.noreply.github.com>
2023-09-12 00:41:58 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 const express = require('express');
 const app = express();
 const moment = require('moment');
-
+app.use(express.json());
 app.get('/api', (req, res) => {
     const slackName = req.query.slack_name;
     const track = req.query.track;
 
-    // Get the current day of the week
     const currentDay = moment().format('dddd');
 
-    // Get the current UTC time with validation of +/-2 minutes
     const currentUtcTime = moment().utc().format('YYYY-MM-DDTHH:mm:ss[Z]');
 
-    // Replace these URLs with your actual GitHub URLs
     const githubFileUrl = 'https://github.com/icemedia001/HNG/blob/main/index.js';
     const githubRepoUrl = 'https://github.com/icemedia001/HNG.git';
 
-    // Create the JSON response
     const response = {
         slack_name: slackName,
         current_day: currentDay,
